3a4fe293ab7c0fdf0b4d28f46181d85f11c4b9c8
sjy <jen.jyseo@gmail.com>
2022-07-07 15:43:05 +0900

    commit

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Row, Col } from "antd";
+import { Row, Col, Spin } from "antd";
 import Main from "./components/pages/main";
 import Login from "./components/pages/login";
 import TableTest from "./components/pages/table";
@@ -14,39 +14,47 @@ import BoardInsert from "./components/pages/board/insert";
 import Header from "./components/templates/Header";
 import Footer from "./components/templates/Footer";
 
+import { useSelector } from "react-redux";
+
 export default function App() {
+  // store에 접근하여 state에 들어있는 값을 가져올 수 있다
+  //reduce에 선언해놓은 name이 common인 값을 가져와서 common 안의 isLoading 값을 빼온 것이다.
+  const { isLoading } = useSelector((state) => state.common);
+
   return (
     <BrowserRouter>
-      <Row>
-        <Col span={24}>
-          <Row>
-            <Col span={24}>
-              <Header />
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <Routes>
-                <Route path="/main" element={<Main />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/table" element={<TableTest />} />
-                <Route path="/hooks" element={<Hooks />} />
-                <Route path="/antd" element={<Antd />} />
-                <Route path="/board" element={<Board />} />
-                {/* 게시판 상세 페이지 */}
-                <Route path="/board/detail/:id" element={<BoardDetail />} />
-                <Route path="/board/update/:id" element={<BoardUpdate />} />
-                <Route path="/board/insert" element={<BoardInsert />} />
-              </Routes>
-            </Col>
-          </Row>
-          <Row>
-            <Col span={24}>
-              <Footer />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <Spin spinning={isLoading}>
+        <Row>
+          <Col span={24}>
+            <Row>
+              <Col span={24}>
+                <Header />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Routes>
+                  <Route path="/main" element={<Main />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/table" element={<TableTest />} />
+                  <Route path="/hooks" element={<Hooks />} />
+                  <Route path="/antd" element={<Antd />} />
+                  <Route path="/board" element={<Board />} />
+                  {/* 게시판 상세 페이지 */}
+                  <Route path="/board/detail/:id" element={<BoardDetail />} />
+                  <Route path="/board/update/:id" element={<BoardUpdate />} />
+                  <Route path="/board/insert" element={<BoardInsert />} />
+                </Routes>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
+                <Footer />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Spin>
     </BrowserRouter>
   );
 }
